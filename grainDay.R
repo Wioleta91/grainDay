53adc217f6e8ad527c2d233c8587b02b03e587a8
Wioleta91 <wiolarasmus@gmail.com>
2025-07-09 16:38:08 +0200

    Adding new GrainDay files

---
+++ grainDay.R
@@ -0,0 +1,11 @@
+#This program takes in percentage of grain type in a sediment and calculates sediment type
+#The project has to be broken down into small steps
+
+
+library(shiny)
+runExample("01_hello")
+
+
+# Define UI for application that draws a histogram
+# Define server logic required to draw a histogram
+# Run the application 
