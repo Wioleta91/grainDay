cdf1ef6edd883213b49ec443e466144bb7d906d5
Wioleta91 <wiolarasmus@gmail.com>
2025-07-09 20:08:29 +0200

    updating ui of the app

--- grainDay.R
+++ grainDay.R
@@ -1,11 +1,35 @@
 #This program takes in percentage of grain type in a sediment and calculates sediment type
 #The project has to be broken down into small steps
-
+#Sediment classification type
 
 library(shiny)
-runExample("01_hello")
+setwd("~/Documents/grainDay")
 
 
-# Define UI for application that draws a histogram
+# Define UI for grainDay app
+
+ui <- fluidPage(
+
+  #App Title
+  titlePanel(GrainDay),
+  
+  sidebarLayout(
+    inputPanel()
+    
+    
+    
+    
+  )
+)
+
+
+
+
 # Define server logic required to draw a histogram
+
+
+
+
+
+
 # Run the application 
